0f81b706a092fe2ec7ed37904a3f5e0c292260e4
dbhinton <dhinton.us@gmail.com>
2023-01-12 00:17:28 -0500

    added test

--- backend/test/departments.test.ts
+++ backend/test/departments.test.ts
@@ -1,8 +1,8 @@
+import axios from 'axios';
 // import { Response, Request } from './../../frontend/src/utils/_abstract';
 import { expect } from 'chai';
 import { it, describe } from 'mocha'
 import { agent as request } from 'supertest';
-
 
 import Application from '../src/app';
 const app = new Application()
@@ -23,9 +23,14 @@ describe('#departments_employees', () => {
     // let response: Response
     let department_id: string = '2'
     it('Should return success', async () => {
-        const res = await request(app.service).get(`/v1/employees/${department_id}`).send();
+        const res = await axios.get(`http://localhost:4324/v1/departments/${department_id}`);
+        console.log(res.data)
         expect(res.status).to.equal(200);
-        expect(res.body).to.be.a('array')
-        expect(res.body.error).to.be.undefined;
+        expect(res.data).to.be.a('array')
+        expect(res.data.error).to.be.undefined;
+        // Expect the array to contain objects with properties  'birthday', 'bio', 'id', 'name', and 'department'
+        expect(res.data[0]).to.have.all.keys('bio', 'birthday','id', 'departmentId', 'name');
+        // // Expect the 'department' property of the first object to equal the passed department id
+        expect(res.data[0]).to.have.property('departmentId', '2')
     })
 })
--- backend/test/employees.test.ts
+++ backend/test/employees.test.ts
@@ -2,6 +2,7 @@
 import { expect } from 'chai';
 import { it, describe } from 'mocha'
 import { agent as request } from 'supertest';
+import axios from 'axios';
 
 
 import Application from '../src/app';
@@ -19,4 +20,21 @@ describe('#employees', () => {
         expect(res.body[0].id).to.equal('82837')
         expect(res.body.error).to.be.undefined;
     })
+})
+
+describe('#employees_id', () => {
+    // let response: Response
+    let employee_id: string = '82338'
+    it('Should return success', async () => {
+        const res = await axios.get(`http://localhost:4324/v1/employees/${employee_id}`);
+        console.log(res.data)
+        expect(res.status).to.equal(200);
+        expect(res.data).to.be.a('array')
+        expect(res.data.error).to.be.undefined;
+        // Expect the array to contain objects with properties 'birthday', 'bio', 'id', 'name', and 'department'
+        expect(res.data[0]).to.have.all.keys('id', 'name', 'departmentId', 'birthday', 'bio');
+
+        // Expect the 'department' property of the first object to equal the passed department id
+        expect(res.data[0]).to.have.property('departmentId', '2');
+   })
 })
